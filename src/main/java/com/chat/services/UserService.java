3e03582952ec5764cbc32d372d584f258a8cc9c7
FatimaEzzahraZD <faezz.zd@gmail.com>
2020-05-11 23:07:04 +0000

    add Autowired user service

--- src/main/java/com/chat/services/UserService.java
+++ src/main/java/com/chat/services/UserService.java
@@ -2,6 +2,7 @@ package com.chat.services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.chat.entities.User;
@@ -11,6 +12,7 @@ import com.chat.repositories.IUserRepository;
 @Service
 public class UserService implements IUserService {
 
+	@Autowired
 	private IUserRepository repo;
 	@Override
 	public User saveOrUpdate(User user) {
